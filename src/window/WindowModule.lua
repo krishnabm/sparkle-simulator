cb013ef1d8fbf82b075d90c9a6130d6338a0c290
Krishna BM <krishna.bm1995@gmail.com>
2023-06-04 00:57:27 +0530

    updated window module

--- src/window/WindowModule.lua
+++ src/window/WindowModule.lua
@@ -1,23 +1,11 @@
 local WindowModule = {}
 
-WindowModule.win = {}
-
-WindowModule.createWindow = function(properties)
-  if not properties then
-    WindowModule.win = am.window(properties);
-  else
-    WindowModule.win = am.window({
-      title = "Hi",
-      width = 400,
-      height = 300,
-      clear_color = vec4(0.5, 0, 0.5, 1)
-    });
-  end
-  WindowModule.win.scene = am.group();
-  return WindowModule.win;
-end
-
-WindowModule.createWindow({})
-
+WindowModule.win = am.window {
+  title = "Strike Shock",
+  width = 800,
+  height = 600,
+  clear_color = vec4(0.5, 0, 0.5, 1)
+}
+WindowModule.win.scene = am.group();
 
 return WindowModule
